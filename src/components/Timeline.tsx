dd0888ec026c9e9cda46f0b8265b067c2f709565
Dmitry Ryzhkov <dimaryz@gmail.com>
2020-09-08 21:40:27 -0700

    timeline updates

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { getTrips, Trip } from '../services/tripService';
+import Badge from 'react-bootstrap/Badge';
+import Card from 'react-bootstrap/Card';
 import './Timeline.css';
 
 export function Timeline() {
@@ -12,25 +14,37 @@ export function Timeline() {
     function renderTrips() {
         return trips.map((t: Trip, index: number) => {
             return (
-                <Link to={`/details/${t.id}`} key={t.id}>
-                    <div
-                        className={
-                            index % 2 === 0 ? 'record left' : 'record right'
-                        }
-                    >
-                        <div className="trip">
-                            <h2>{t.title}</h2>
-                            <h3>{t.days[0].date.toDateString()}</h3>
+                <>
+                    <Link to={`/details/${t.id}`} key={t.id}>
+                        <div
+                            className={
+                                index % 2 === 0 ? 'record left' : 'record right'
+                            }
+                        >
+                            <Card className="trip">
+                                <Card.Body>
+                                    <Card.Title>{t.title}</Card.Title>
+                                    <Card.Text>
+                                        {t.days[0].date.toDateString()}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
                         </div>
-                    </div>
-                </Link>
+                    </Link>
+
+                    {index === 0 && (
+                        <Badge className="center" variant="info" as="div">
+                            2020
+                        </Badge>
+                    )}
+                </>
             );
         });
     }
 
     return (
         <div id="adv">
-            <h2 id="page-title">Adventure Timeline</h2>
+            {/* <h2 id="page-title">Adventure Timeline</h2> */}
             <div className="timeline">{renderTrips()}</div>
         </div>
     );
