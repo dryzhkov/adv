2e517b978f00c4d7a45cb022ad41c2bd9fbc08ce
Dmitry Ryzhkov <dimaryz@gmail.com>
2020-09-09 20:38:54 -0700

    netlify

--- functions/trips/trips.js
+++ functions/trips/trips.js
@@ -1,6 +1,22 @@
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+const { MONGODB_USER, MONGODB_PSWD, MONGODB_NAME, MONGODB_URI } = process.env;
+const mongoURI = `mongodb+srv://${MONGODB_USER}:${MONGODB_PSWD}@${MONGODB_URI}/${MONGODB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(mongoURI, { useNewUrlParser: true });
+
 exports.handler = function (event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(event),
+    client.connect((err) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        const collection = client.db('motolog').collection('trips');
+        collection.find().toArray((err, result) => {
+            callback(null, {
+                statusCode: 200,
+                body: JSON.stringify(result),
+            });
+            client.close();
+        });
     });
 };
